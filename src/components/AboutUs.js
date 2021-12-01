96ca30447e5bba272288262b9dacfc9f7eaaeff1
Klinsman <lauklinsman@gmail.com>
2021-12-01 14:09:21 +0800

    first commit

---
+++ src/components/AboutUs.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './AboutUs.css'
+import { Button } from './Button'
+
+function AboutUs() {
+    return (
+        <div className='about__us'>
+            <h1>About Us</h1>
+            <h2>Making Your Brand Shine.</h2>
+            <p>RLS Advertising has assisted businesses in strategizing and implementing their creative objectives based on customers' needs. Our expanding Online Marketing Team is run by enthusiastic thinkers that collaborate directly with each brand to develop strategies that achieve unrivalled results.</p>
+            <br />
+            <div className='about__btn'>
+                <Button
+                className='btns'
+                buttonStyle='btn--outline'
+                buttonSize='btn--large'
+                >
+                    <span>Find Out More</span>
+                </Button>
+            </div>  
+        </div>
+    )
+}
+
+export default AboutUs
---
+++ src/components/HeroSection.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+import Typical from 'react-typical';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <video src='/videos/video.mp4' autoPlay loop muted />
+      <h1>Propel your brands with</h1> <br/><br/><br/>
+      <h1><i>RLS Advertising</i> <sup><i class="far fa-copyright"></i></sup>  </h1> 
+      <p>We Can Help You To{' '}
+        <Typical 
+          loop={Infinity}
+          wrapper="b"
+          steps={[
+            'Promote',
+            1200,
+            'Advertise',
+            1200,
+            'Aspire',
+            1200
+          ]}
+        />
+        Your Business
+      </p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+        >
+          <span>Contact Us</span>
+        </Button>
+        {/* <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          WATCH TRAILER <i className='far fa-play-circle' />
+        </Button> */}
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
