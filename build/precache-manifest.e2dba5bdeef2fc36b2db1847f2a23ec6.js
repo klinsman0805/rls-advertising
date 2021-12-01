0ab5b7b29e8aa06f1a75719fa7b6763796c0a113
Klinsman <lauklinsman@gmail.com>
2021-12-01 15:04:01 +0800

    second commit

---
+++ build/precache-manifest.e2dba5bdeef2fc36b2db1847f2a23ec6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edbdfbf5d1a3e97833107cb5d30e605d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac1e642c71ea69fb33d3",
+    "url": "/static/css/main.042f3a29.chunk.css"
+  },
+  {
+    "revision": "34e62ee662dc792aaf77",
+    "url": "/static/js/2.a5def17e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a5def17e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac1e642c71ea69fb33d3",
+    "url": "/static/js/main.fbe7b812.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
